57b82cde63f5316e56a0dcbd423f3933079f9b15
andrewteece <andrewteece@gmail.com>
2026-03-23 14:29:29 -0500

    chore: merge Next.js config into next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,20 @@ const withMDX = createMDX({ extension: /\.mdx?$/ });
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'mdx'],
   images: {
+    // Enable Next/Vercel optimizer + modern formats
     formats: ['image/avif', 'image/webp'],
+    deviceSizes: [360, 640, 768, 1024, 1280, 1536, 1920],
+    imageSizes: [320, 420, 640, 750, 828, 1080, 1200],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'andrewteecephotography.com' }],
+        destination: 'https://www.andrewteecephotography.com/:path*',
+        permanent: true,
+      },
+    ];
   },
 } satisfies NextConfig;
 
